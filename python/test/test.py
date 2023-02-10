8d41465b9df3fd5d768c247f44b2c6832e5ae093
ckafff <borisnovoselov602@gmail.com>
2023-02-10 04:36:41 +0300

    fix tests

--- python/test/test.py
+++ python/test/test.py
@@ -2,13 +2,13 @@ import pytest
 import os
 
 def path_to_bin():
-    stream = os.popen('cabal exec which haskell-lab3-exe')
+    stream = os.popen('cabal exec which haskell-lab4-exe')
     bin_p = stream.read()
     bin_p = bin_p.replace('\n', '')
     return bin_p
 
 def test_1():
-    os.popen(path_to_bin() +' --use /test_files/left.use --dox /test_files/left.dox')
+    os.popen(path_to_bin() +' --use ./test_files/left.use --dox ./test_files/left.dox')
     file1 = open("./python/test/expected_results/lu_ld_diff.md", "r")
     file2 = open("diff.md", "r")
     expected = file1.read()
@@ -17,7 +17,7 @@ def test_1():
 
 
 def test_2():
-    os.popen(path_to_bin() +' --use /test_files/right.use --dox /test_files/left.dox')
+    os.popen(path_to_bin() +' --use ./test_files/right.use --dox ./test_files/left.dox')
     file1 = open("./python/test/expected_results/ru_ld_diff.md", "r")
     file2 = open("diff.md", "r")
     expected = file1.read()
@@ -25,7 +25,7 @@ def test_2():
     assert(res == expected)
 
 def test_3():
-    os.popen(path_to_bin() +' --use /test_files/left.use --dox /test_files/right.dox')
+    os.popen(path_to_bin() +' --use ./test_files/left.use --dox ./test_files/right.dox')
     file1 = open("./python/test/expected_results/lu_rd_diff.md", "r")
     file2 = open("diff.md", "r")
     expected = file1.read()
@@ -33,7 +33,7 @@ def test_3():
     assert(res == expected)
 
 def test_4():
-    os.popen(path_to_bin() +' --use /test_files/other_right.use --dox /test_files/right.dox')
+    os.popen(path_to_bin() +' --use ./test_files/other_right.use --dox ./test_files/right.dox')
     file1 = open("./python/test/expected_results/ou_rd_diff.md", "r")
     file2 = open("diff.md", "r")
     expected = file1.read()
@@ -43,13 +43,13 @@ def test_4():
 def test_5():
     stream = os.popen("rm -f diff.md; rm diff.md")
     expected = stream.read()
-    os.popen(path_to_bin() +' --use /test_files/right.use --dox /test_files/right.dox')
+    os.popen(path_to_bin() +' --use ./test_files/right.use --dox ./test_files/right.dox')
     stream = os.popen("rm diff.md")
     res = stream.read()
     assert(res == expected)
 
 def test_6():
-    os.popen(path_to_bin() +' --dox /test_files/right.dox --gu')
+    os.popen(path_to_bin() +' --dox ./test_files/right.dox --gu')
     file1 = open("./python/test/expected_results/generated.use", "r")
     file2 = open("generated.use", "r")
     expected = file1.read()
