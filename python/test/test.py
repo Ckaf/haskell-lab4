1d8d990e89d8beafc465f397b0a42b3d9d3f9106
ckafff <borisnovoselov602@gmail.com>
2023-02-10 05:00:03 +0300

    sleep in tests

--- python/test/test.py
+++ python/test/test.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import time
 
 def path_to_bin():
     stream = os.popen('cabal exec which haskell-lab4-exe')
@@ -9,6 +10,7 @@ def path_to_bin():
 
 def test_1():
     os.popen(path_to_bin() +' --use ./test_files/left.use --dox ./test_files/left.dox')
+    time.sleep(1)
     file1 = open("./python/test/expected_results/lu_ld_diff.md", "r")
     file2 = open("diff.md", "r")
     expected = file1.read()
@@ -18,6 +20,7 @@ def test_1():
 
 def test_2():
     os.popen(path_to_bin() +' --use ./test_files/right.use --dox ./test_files/left.dox')
+    time.sleep(1)
     file1 = open("./python/test/expected_results/ru_ld_diff.md", "r")
     file2 = open("diff.md", "r")
     expected = file1.read()
@@ -26,6 +29,7 @@ def test_2():
 
 def test_3():
     os.popen(path_to_bin() +' --use ./test_files/left.use --dox ./test_files/right.dox')
+    time.sleep(1)
     file1 = open("./python/test/expected_results/lu_rd_diff.md", "r")
     file2 = open("diff.md", "r")
     expected = file1.read()
@@ -34,6 +38,7 @@ def test_3():
 
 def test_4():
     os.popen(path_to_bin() +' --use ./test_files/other_right.use --dox ./test_files/right.dox')
+    time.sleep(1)
     file1 = open("./python/test/expected_results/ou_rd_diff.md", "r")
     file2 = open("diff.md", "r")
     expected = file1.read()
@@ -44,12 +49,14 @@ def test_5():
     stream = os.popen("rm -f diff.md; rm diff.md")
     expected = stream.read()
     os.popen(path_to_bin() +' --use ./test_files/right.use --dox ./test_files/right.dox')
+    time.sleep(1)
     stream = os.popen("rm diff.md")
     res = stream.read()
     assert(res == expected)
 
 def test_6():
     os.popen(path_to_bin() +' --dox ./test_files/right.dox --gu')
+    time.sleep(1)
     file1 = open("./python/test/expected_results/generated.use", "r")
     file2 = open("generated.use", "r")
     expected = file1.read()
